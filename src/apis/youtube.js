d177f33928e4ee09b699e7b6f6efc532f91c7260
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-02 08:19:06 +0900

    Instll axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
 const KEY = ""; //次回貼り付け
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+    q: "surfboards",
+  },
+});
