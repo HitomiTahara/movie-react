f128a77c1579e344c2328e1af4939e377420877b
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-04 16:25:33 +0900

    Created VideoItem for display videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const KEY = ""; //次回貼り付け
+
+const KEY = "AIzaSyAOqQ39tv0E6BstOI-ODczCcJ9mgTU3R4U"; //次回貼り付け
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
