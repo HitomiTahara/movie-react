f9236ba061fd95edfb97d922f06b1c7add160b15
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-05 09:00:51 +0900

    Fix 'マージ競合マーカーが検出されました '

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const KEY = "AIzaSyAOqQ39tv0E6BstOI-ODczCcJ9mgTU3R4U"; //次回貼り付け
-=======
 const KEY = ""; //次回貼り付け
->>>>>>> e6d4105 (Again Created VideoItem for display videos)
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
