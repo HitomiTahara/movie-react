25b83517fbeeaaa2ef623a86d682aad2350423f9
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-06 10:45:26 +0900

    Install dotenv - All done Finally

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const KEY = ""; //次回貼り付け
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     type: "video",
     maxResults: 5,
-    key: KEY,
+    key: process.env.REACT_APP_API_KEY,
   },
 });
