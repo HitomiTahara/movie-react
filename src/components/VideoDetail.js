dd99eca6e15e3dfacbd0ac7f24ccb96910bca88b
HitomiTahara <hitomi.20220218@gmail.com>
2022-10-06 09:30:03 +0900

    Fixed some issue and error
    Add key src id etc...

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,9 +4,14 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
       <div className="ui segment">
+        <div className="ui embed">
+          <iframe title="video player" src={videoSrc} />
+        </div>
         <h4 className="ui header">{video.snippet.title}</h4>
         <p className="ui">{video.snippet.description}</p>
       </div>
